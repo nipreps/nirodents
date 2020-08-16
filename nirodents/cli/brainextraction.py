a51d6dbccbe1f52fc700b4b18a378883b180d15e
Oscar Esteban <code@oscaresteban.es>
2020-08-16 08:01:34 -0700

    enh: set better default parameters for antsAI in CLI

--- nirodents/cli/brainextraction.py
+++ nirodents/cli/brainextraction.py
@@ -109,7 +109,7 @@ ANTs-based Rodents ToolS (ARTs) package.\
         dest="antsai_step",
         action="store",
         type=float,
-        default=2,
+        default=4,
         help="Specify ants-ai step size parameter",
     )
 
@@ -119,7 +119,7 @@ ANTs-based Rodents ToolS (ARTs) package.\
         action="store",
         nargs=3,
         type=float,
-        default=[0, 0, 0],
+        default=(0, 4, 4),
         help="Specify ants-ai Search Grid parameter",
     )
     return parser
